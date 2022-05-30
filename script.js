3d07d23b260709309580b9af445f803c52380cba
EdelDenis <edeldenis12@gmail.com>
2022-05-30 10:53:55 +0000

    h

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const inputRub = document.querySelector("#rub");
+const inputUsd = document.querySelector("#usd");
+
+inputRub.addEventListener("input",()=>{
+    const request = new XMLHttpRequest();
+
+    request.open("GET","current.json");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    request.send();
+
+    request.addEventListener("load", ()=>{
+          if(request.status === 200){
+              const data = JSON.parse(request.response);
+              inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+          } else {
+              console.log(inputUsd.value = "Что то пошло не так")
+          }
+    });
+
+   
+    // status
+    // statusText
+    // response
+    // readyState
+
+
+})
